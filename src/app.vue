198c6b99b1c7ba96baadb879f7f6f62f4c5acbdf
lupfeliz <lupfeliz@gmail.com>
2024-10-11 03:00:10 +0900

    svg 애니메이션 테스트

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,5 @@
 <script setup>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
 import 'bootstrap/dist/css/bootstrap.css'
-
-const chartData = ref({
-  labels: [],
-  datasets: [{
-    data: [],
-    backgroundColor: [],
-  }]
-})
 
 const colors = [
   '#36A2EB',
@@ -23,41 +11,16 @@ const colors = [
   '#C9CBCF'
 ]
 
-for (let inx = 0; inx < 30; inx++) {
-  const label = `D-${inx}`
-  chartData.value.labels.push(label)
-  chartData.value.datasets[0].data.push(inx)
-  chartData.value.datasets[0].backgroundColor.push(colors[inx % colors.length])
-}
-
-const chartOptions = {
-  responsive: true,
-  animation: {
-    duration: 10
-  }
-}
-
-const barchart = ref()
-
-const myStyles = computed(() => ({
-  // backgroundColor: '#f00'
-}))
-
-const swap = (model, inx1, inx2) => {
-  const o = model[inx1]
-  model[inx1] = model[inx2]
-  model[inx2] = o
-}
-
 onMounted(async () => {
-  console.log('CHART:', barchart.value)
-  setTimeout(async () => {
-    swap(chartData.value.datasets[0].data, 1, 10)
-    swap(chartData.value.datasets[0].backgroundColor, 1, 10)
-    swap(chartData.value.labels, 1, 10)
-    chartData.value = JSON.parse(JSON.stringify(chartData.value))
-    console.log('OK')
-  }, 3000)
+  const g1 = document.querySelector('#g1')
+  const r1 = document.querySelector('#g1 > rect')
+  g1.classList.add('svg-animation')
+  r1.classList.add('svg-animation')
+
+  setTimeout(() => {
+    r1.style.transform = `rotate(${90}deg)`
+    g1.style.transform = `translate(${30}px, ${0}px)`
+  }, 500)
 })
 
 </script>
@@ -65,14 +28,18 @@ onMounted(async () => {
   <div class="container">
     <h3>GRAPH</h3>
     <div>
-      <Bar
-        ref="barchart"
-        id="my-chart-id"
-        aria-label="Sales figures for the years 2022 to 2024. Sales in 2022: 987, Sales in 2023: 1209, Sales in 2024: 825."
-        :options="chartOptions"
-        :data="chartData"
-        :style="myStyles"
-        />
+      <svg viewBox="0 0 100 100" width="100%">
+        <g id="g1">
+          <rect x="0" y="0" width="10" height="10" fill="navy">
+          </rect>
+        </g>
+      </svg>
     </div>
   </div>
 </template>
+<style scoped>
+.svg-animation {
+  transition: 0.5s ease-in-out;
+  transition-timing-function: cubic-bezier(0.39, 0.575, 0.565, 1);
+}
+</style>
