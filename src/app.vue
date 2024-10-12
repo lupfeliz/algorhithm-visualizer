08e95f3aaa82548f5e0f40a2e1d20443557d465e
lupfeliz <lupfeliz@gmail.com>
2024-10-13 06:05:31 +0900

    정렬 알고리즘 구현 및 visualizing 테스트

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,25 @@
 <script setup>
-import * as Tone from "tone"
-import $ from 'jquery'
+// import * as Tone from "tone"
+import { SortableArray, Sorter } from '@/libs/sort'
+// import $ from 'jquery'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
+const visual = ref({
+  vlist: [],
+  plist: [],
+  plist: [],
+  clist: [],
+  focus: [],
+  comphist: [],
+  swaphist: [],
+})
+const audio = {
+  ctx: undefined,
+  vol: undefined,
+  handle: [],
+  freq: []
+}
 const colors = [
   '#36A2EB',
   '#FF6384',
@@ -14,62 +30,225 @@ const colors = [
   '#C9CBCF'
 ]
 
-const audio = {
-  ctx: undefined,
-  vol: undefined
+const init = (qty, max) => {
+  /** AudioContext 는 반드시 키보드입력이 먼저 수행되어져야 활성화 된다. */
+  if (!audio.ctx) {
+    audio.ctx = new AudioContext()
+    audio.vol = audio.ctx.createGain()
+    audio.vol.connect(audio.ctx.destination)
+    audio.vol.gain.value = 0.1
+  }
+  max = max - 10
+  const clist = visual.value.clist = []
+  const ilist = visual.value.plist = []
+  const plist = visual.value.plist = []
+  const vlist = visual.value.vlist = []
+  const focus = visual.value.focus = []
+  const swaphist = visual.value.swaphist = []
+  const comphist = visual.value.comphist = []
+  for (let inx = 0; inx < qty; inx++) {
+    const v = (Math.round(Math.random() * max) + 1)
+    ilist.push(inx)
+    plist.push(inx)
+    vlist.push(v)
+    clist.push(colors[inx % colors.length])
+  }
+  comphist.push([])
+  swaphist.push(JSON.parse(JSON.stringify(plist)))
+  const sdata = JSON.parse(JSON.stringify(vlist))
+  const sortable = new SortableArray(sdata, function(type, p1, p2) {
+    switch (type) {
+    case 'compare': {
+      // console.log(`COMPARE:[${p1}]=${sdata[p1]} : [${p2}]=${sdata[p2]}`)
+      comphist.push([plist[p1], plist[p2]])
+      swaphist.push(JSON.parse(JSON.stringify(plist)))
+    } break
+    case 'swap': {
+      // console.log(`SWAP:[${p1}]=${sdata[p1]} : [${p2}]=${sdata[p2]} -> [${p1}]=${sdata[p2]} : [${p2}]=${sdata[p1]} `)
+      const v = plist[p1]
+      plist[p1] = plist[p2]
+      plist[p2] = v
+      comphist.push([plist[p1], plist[p2]])
+      swaphist.push(JSON.parse(JSON.stringify(plist)))
+    } break
+    default: console.log(...arguments) }
+  })
+  console.log('BEFORE:', JSON.stringify(sdata))
+  // Sorter.heapsort(sortable)
+  Sorter.quicksort(sortable)
+  // Sorter.bubblesort(sortable)
+  // Sorter.selectionsort(sortable)
+  console.log('AFTER:', JSON.stringify(sdata))
+  visual.value.plist = swaphist[0]
+  const play = (hinx) => {
+    visual.value.plist = swaphist[hinx]
+    visual.value.vlist = JSON.parse(JSON.stringify(vlist))
+    const focus = visual.value.focus = comphist[hinx]
+    for (let inx = 0; inx < focus.length; inx++) {
+      // console.log(`FOCUS:${vlist[focus[inx]]}`)
+      tone(vlist[focus[inx]], inx)
+    }
+    if (hinx + 1 < swaphist.length) {
+      setTimeout(() => play(hinx + 1), 100)
+    }
+  }
+  setTimeout(() => play(0), 1000)
 }
 
-onMounted(async () => {
-  const g1 = document.querySelector('#g1')
-  const r1 = document.querySelector('#g1 > rect')
-  g1.classList.add('svg-animation')
-  r1.classList.add('svg-animation')
-  setTimeout(() => {
-    r1.style.transform = `rotate(${90}deg)`
-    g1.style.transform = `translate(${30}px, ${0}px)`
+const tone = (freq, slot) => {
+  if (audio.freq[slot]) {
+    if (audio.freq[slot] != freq) {
+      audio.handle[slot].stop()
+      audio.handle[slot] = undefined
+    }
+  }
+  const osc = audio.ctx.createOscillator();
+  osc.connect(audio.vol);
+  /** sine, square, sawtooth, triangle */
+  osc.type = 'sine'
+  osc.frequency.value = freq;
+  osc.start();
+  audio.handle[slot] = osc
+  audio.freq[slot] = freq
+  setTimeout(() => osc.stop(), 100)
+}
 
-    const synth = new Tone.Synth().toDestination()
-    const now = Tone.now()
-    synth.volume.value = -10
-    synth.triggerAttackRelease('C4', '16n', now)
-    synth.triggerAttackRelease('E4', '16n', now + 0.25)
-    synth.triggerAttackRelease('G4', '16n', now + 0.5)
-  }, 2000)
-  /** AudioContext 는 반드시 키보드입력이 먼저 수행되어져야 활성화 된다. */
-  // {
-  //   audio.ctx = new AudioContext()
-  //   audio.vol = audio.ctx.createGain()
-  //   audio.vol.connect(audio.ctx.destination)
-  //   audio.vol.gain.value = 0.5
-  // }
-})
-
-// const playTone = (freq) => {
-//   const osc = audio.ctx.createOscillator();
-//   osc.connect(audio.vol);
-//   osc.type = 'square'
-//   osc.frequency.value = freq;
-//   osc.start();
-//   return osc;
-// }
+const width = 1000
+const height = 500
+const qty = 30
 
 </script>
 <template>
   <div class="container">
     <h3>GRAPH</h3>
     <div>
-      <svg viewBox="0 0 400 200" width="100%">
-        <g id="g1">
-          <rect x="0" y="0" width="10" height="10" fill="navy">
-          </rect>
+      <svg :viewBox="`0 0 ${width} ${height}`" width="100%">
+        <template v-for="(v, inx) in visual.vlist">
+        <g
+          :id="`g${inx}`"
+          :style="`transform: translate(${(visual.plist.indexOf(inx)) * Math.floor(1.0 * width / visual.vlist.length)}px, 0px)`"
+          class="svg-animation"
+          >
+          <rect
+            x="0"
+            :y="`${height - v}`"
+            :width="`${Math.floor(1.0 * width / visual.vlist.length) - 2}`"
+            :height="`${v}`"
+            :fill="`${visual.clist[inx]}`"
+            :class="`svg-animation ${visual.focus.indexOf(inx) != -1 ? 'focused' : ''}`"
+            />
         </g>
+        </template>
       </svg>
+    </div>
+    <div class="mt-1">
+      <button
+        class="btn btn-secondary mx-1"
+        @click="() => init(qty, height)"
+        >
+        초기화
+      </button>
+      <button
+        class="btn btn-primary mx-1"
+        @click="test"
+        >
+        시작
+      </button>
+      <button
+        class="btn btn-primary mx-1"
+        @click="sorttest"
+        >
+        중지
+      </button>
+      <button
+        class="btn btn-primary mx-1"
+        >
+        이전
+      </button>
+      <button
+        class="btn btn-primary mx-1"
+        >
+        다음
+      </button>
     </div>
   </div>
 </template>
 <style scoped>
 .svg-animation {
-  transition: 0.5s ease-in-out;
+  transition: transform 0.5s ease-in-out;
   transition-timing-function: cubic-bezier(0.39, 0.575, 0.565, 1);
+}
+
+.mt-1 {
+  margin-top: 0.25rem;
+}
+
+.mx-1 {
+  margin-left: 0.25rem;
+  margin-right: 0.25rem;
+}
+
+.my-1 {
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.text-right {
+  text-align: right;
+}
+
+.overflow-hidden {
+  overflow: hidden;
+}
+
+.overflow-x-auto {
+  overflow-x: auto;
+}
+
+.inline-block {
+  display: inline-block;
+}
+
+.hidden {
+  display: hidden;
+}
+
+.flex {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+}
+
+.justify-center {
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+}
+
+.w-screen {
+  width: 100vw;
+}
+
+.w-full {
+  width: 100%;
+}
+
+.max-w-1\/2 {
+  max-width: calc(50%);
+}
+
+.max-w-2\/3 {
+  max-width: calc(100% * 2 / 3);
+}
+
+rect.focused {
+  stroke: #000;
+  stroke-width: 4px;
 }
 </style>
---
+++ src/libs/sort.ts
@@ -0,0 +1,259 @@
+/**
+ * @File        : sort.ts
+ * @Author      : 정재백
+ * @Since       : 2024-10-12
+ * @Description : 정렬 알고리즘에 필요한 코드 정리
+ * @Site        : https://devlog.ntiple.com
+ **/
+
+import type { Function0, Function1, Function2 } from "lodash"
+
+type SortableType = {
+  length: Function0<number>
+  compare: Function2<number, number, number>
+  swap: Function2<number, number, void>
+  shift: Function2<number, number, void>
+  data?: Function0<any>,
+  intercept?: InterceptType
+}
+
+type ComparableType = number | string | {
+  compare: Function1<ComparableType, number>
+}
+
+type InterceptType = (type: 'compare' | 'swap' | 'shift' | string, ...arg: any) => any
+
+type SorterType = (sortable: SortableType, ascending: boolean) => any
+
+class SortableArray<T extends ComparableType> implements SortableType {
+
+  private _data: Array<T> = undefined as any as Array<T>
+
+  private _intercept: InterceptType = undefined as any as InterceptType
+
+  constructor(data: Array<T>, intercept?: InterceptType) {
+    this._data = data
+    if (intercept) { this._intercept = intercept }
+  }
+
+  length() { return this._data.length }
+
+  compare(inx1: number, inx2: number) {
+    let v1 = this._data[inx1]
+    let v2 = this._data[inx2]
+    if (this._intercept) { this._intercept('compare', inx1, inx2) }
+    if (v1 == v2 && (v1 === undefined || v1 === null)) { return 0 }
+    if (v1 === undefined || v1 === null) { return -1 }
+    if (v2 === undefined || v2 === null) { return 1 }
+    if (typeof v1 !== typeof v2) { return -1 }
+    switch (typeof v1) {
+    case 'number': {
+      if (v1 === v2) {
+        return 0
+      } else if (Number(v1) > Number(v2)) {
+        return 1
+      } else {
+        return -1
+      }
+    } break
+    case 'string': {
+      if (v1 === v2) {
+        return 0
+      } else if (String(v1) > String(v2)) {
+        return 1
+      } else {
+        return -1
+      }
+    } break
+    default: {
+      console.log('V1:', v1)
+      return v1.compare(v2)
+    } }
+    return 0
+  }
+
+  swap(inx1: number, inx2: number) {
+    if (this._intercept) { this._intercept('swap', inx1, inx2) }
+    if (inx1 >= 0 && inx2 >= 0 && inx1 < this.length() && inx2 < this.length()) {
+      const v = this._data[inx1]
+      this._data[inx1] = this._data[inx2]
+      this._data[inx2] = v
+    }
+  }
+
+  shift(inx: number, length: number) {
+    if (this._intercept) { this._intercept('shift', inx, length) }
+    if (inx >= 0 && length >= 0 && (inx + length) < this.length()) {
+      const v = this._data[inx]
+      this._data.splice(inx, 1)
+      this._data.splice(inx + length, 0, v)
+    }
+  }
+
+  data() { return this._data }
+
+  intercept(type: string, ...arg: any) {
+    if (this._intercept) { this._intercept(type, ...arg) }
+  }
+}
+
+/** 버블정렬 */
+const bubblesort: SorterType = (sortable: SortableType, ascending: boolean = true) => {
+  if (sortable.intercept) { sortable.intercept('start') }
+  let len = sortable.length()
+  let complete
+  do {
+    complete = true
+    for (let inx = 0; inx < len - 1; inx++) {
+      let comp = sortable.compare(inx, inx + 1)
+      if ((ascending && comp > 0) || (!ascending && comp < 0)) {
+        sortable.swap(inx, inx + 1)
+        complete = false
+      }
+    }
+    if (sortable.intercept) { sortable.intercept('complete', complete) }
+  } while (!complete)
+}
+
+/** 선택정렬 */
+const selectionsort: SorterType = (sortable: SortableType, ascending: boolean = true) => {
+  if (sortable.intercept) { sortable.intercept('start') }
+  let len = sortable.length()
+  let loops = len - 1
+  for (let inx1 = 0; inx1 < loops; inx1++) {
+    for (let inx2 = inx1 + 1; inx2 < len; inx2++) {
+      let comp = sortable.compare(inx1, inx2)
+      if ((ascending && comp > 0) || (!ascending && comp < 0)) {
+        sortable.swap(inx1, inx2)
+      }
+    }
+  }
+  if (sortable.intercept) { sortable.intercept('complete') }
+}
+
+/** 퀵정렬 */
+const quicksort: SorterType = (sortable: SortableType, ascending: boolean = true) => {
+  const _quicksort = (sortable: SortableType, left: number, right: number, ascending: boolean) => {
+    // if (sortable.intercept) { sortable.intercept('call', left, right) }
+    let diff = right - left
+    let comp
+    if (diff == 1) {
+      comp = sortable.compare(left, right)
+      if ((ascending && comp > 0) || (!ascending && comp < 0)) {
+        sortable.swap(left, right)
+      }
+    } else if (diff > 1) {
+      let linx = left
+      let rinx = right
+      let pivot = (left + right) >> 1
+      LOOP1: do {
+        LOOP2: do {
+          comp = sortable.compare(linx, pivot);
+          if ((ascending && comp < 0) || (!ascending && comp > 0)) {
+            linx += 1
+          } else {
+            break LOOP2
+          }
+        } while(true)
+        LOOP3: do {
+          comp = sortable.compare(rinx, pivot)
+          if ((ascending && comp > 0) || (!ascending && comp < 0)) {
+            rinx -= 1
+          } else {
+            break LOOP3
+          }
+        } while(true)
+        if (linx <= rinx) {
+          if (linx != rinx) {
+            if (linx == pivot) {
+              pivot = rinx
+            } else if (rinx == pivot) {
+              pivot = linx
+            }
+            sortable.swap(linx, rinx)
+          }
+          linx += 1
+          rinx -= 1
+        }
+        if (linx > rinx) { break LOOP1 }
+      } while(true)
+      if (left < rinx) { _quicksort(sortable, left, rinx, ascending) }
+      if (linx < right) { _quicksort(sortable, linx, right, ascending) }
+    }
+    // if (sortable.intercept) { sortable.intercept('return', left, right) }
+  }
+  let len = sortable.length()
+  if (sortable.intercept) { sortable.intercept('start') }
+  _quicksort(sortable, 0, len - 1, ascending)
+  if (sortable.intercept) { sortable.intercept('complete') }
+}
+
+const heapsort: SorterType = (sortable: SortableType, ascending: boolean = true) => {
+
+  const _make_heap = (sortable: SortableType, top: number, size: number, ascending: boolean) => {
+    let pos = size >> 1
+    /** make heap tree from base of the tree */
+    for (let inx = pos; inx >= top + 1; inx--) {
+      _heapify(sortable, inx, pos, size, ascending)
+    }
+  }
+
+  const _heapify = (sortable: SortableType, inx: number, pos: number, size: number, ascending: boolean) => {
+    pos = pos + 1
+    let is_bin = false
+    let child: number
+    let comp: number
+    while (inx < pos) {
+      let parent = inx - 1
+      /** top * 2 */
+      let child1 = (inx << 1) - 1
+      /** top * 2 + 1 */
+      let child2 = child1 + 1
+      if (child2 < size) {
+        comp = sortable.compare(child1, child2)
+        if ((ascending && comp > 0) || (!ascending && comp < 0)) {
+          child = child1
+        } else {
+          child = child2
+        }
+      } else {
+        child = child1
+      }
+      comp = sortable.compare(parent, child)
+
+      if ((ascending && comp < 0) || (!ascending && comp > 0)) {
+        sortable.swap(parent, child)
+        is_bin = true
+        inx = child + 1
+      } else {
+        break
+      }
+    }
+    return is_bin
+  }
+  {
+    let pos: number
+    let size = sortable.length()
+    _make_heap(sortable, 0, size, ascending)
+    while (size > 1) {
+      /** take node on top */
+      size -= 1
+      pos = size >> 1
+      sortable.swap(0, size)
+      _heapify(sortable, 1, pos, size, ascending)
+    }
+  }
+}
+
+const Sorter = {
+  bubblesort,
+  selectionsort,
+  quicksort,
+  heapsort
+}
+
+export {
+  SortableArray,
+  Sorter,
+  type SortableType, type ComparableType, type InterceptType, type SorterType
+}
