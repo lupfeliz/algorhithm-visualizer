f48b5c4b84a6e4cdf91e88e6ab55a9efcc8f015f
lupfeliz <lupfeliz@gmail.com>
2024-10-14 16:07:11 +0900

    소스 주석 추가

--- src/libs/sort.ts
+++ src/libs/sort.ts
@@ -9,11 +9,17 @@
 import type { Function0, Function1, Function2 } from "lodash"
 
 type SortableType = {
+  /** 데이터 길이 */
   length: Function0<number>
+  /** n1, n2 번째 데이터의 비교 같으면 0, n1 이 크면 1, n2 가 크면 -1  */
   compare: Function2<number, number, number>
+  /** n1, n2 번째 데이터의 위치를 서로 바꾼다  */
   swap: Function2<number, number, void>
+  /** 데이터 shift 연산 */
   shift: Function2<number, number, void>
+  /** 데이터 반환 (선택구현) */
   data?: Function0<any>,
+  /** 상황별 중간삽입 프로세스 (선택구현) */
   intercept?: InterceptType
 }
 
@@ -26,18 +32,16 @@ type InterceptType = (type: 'compare' | 'swap' | 'shift' | string, ...arg: any) 
 type SorterType = (sortable: SortableType, ascending: boolean) => any
 
 class SortableArray<T extends ComparableType> implements SortableType {
-
+  /** 데이터 */
   private _data: Array<T> = undefined as any as Array<T>
-
+  /** 중간삽입 프로세스 */
   private _intercept: InterceptType = undefined as any as InterceptType
-
+  /** 생성자, 데이터와 중간삽입 프로세스를 받는다  */
   constructor(data: Array<T>, intercept?: InterceptType) {
     this._data = data
     if (intercept) { this._intercept = intercept }
   }
-
   length() { return this._data.length }
-
   compare(inx1: number, inx2: number) {
     let v1 = this._data[inx1]
     let v2 = this._data[inx2]
@@ -71,7 +75,6 @@ class SortableArray<T extends ComparableType> implements SortableType {
     } }
     return 0
   }
-
   swap(inx1: number, inx2: number) {
     if (this._intercept) { this._intercept('swap', inx1, inx2) }
     if (inx1 >= 0 && inx2 >= 0 && inx1 < this.length() && inx2 < this.length()) {
@@ -80,7 +83,6 @@ class SortableArray<T extends ComparableType> implements SortableType {
       this._data[inx2] = v
     }
   }
-
   shift(inx: number, length: number) {
     if (this._intercept) { this._intercept('shift', inx, length) }
     if (inx >= 0 && length >= 0 && (inx + length) < this.length()) {
@@ -89,9 +91,7 @@ class SortableArray<T extends ComparableType> implements SortableType {
       this._data.splice(inx + length, 0, v)
     }
   }
-
   data() { return this._data }
-
   intercept(type: string, ...arg: any) {
     if (this._intercept) { this._intercept(type, ...arg) }
   }
@@ -106,7 +106,8 @@ const bubblesort: SorterType = (sortable: SortableType, ascending: boolean = tru
     complete = true
     len -= 1
     for (let inx = 0; inx < len; inx++) {
-      let comp = sortable.compare(inx, inx + 1)
+      /** 바로 옆자리 데이터와 비교 */
+      const comp = sortable.compare(inx, inx + 1)
       if ((ascending && comp > 0) || (!ascending && comp < 0)) {
         sortable.swap(inx, inx + 1)
         complete = false
@@ -123,6 +124,7 @@ const selectionsort: SorterType = (sortable: SortableType, ascending: boolean = 
   let end = len - 1
   for (let inx1 = 0; inx1 < end; inx1++) {
     for (let inx2 = inx1 + 1; inx2 < len; inx2++) {
+      /** n번째 데이터 를 나머지 n번째 이후 전체 데이터와 각각 비교한다  */
       let comp = sortable.compare(inx1, inx2)
       if ((ascending && comp > 0) || (!ascending && comp < 0)) {
         sortable.swap(inx1, inx2)
@@ -180,6 +182,7 @@ const quicksort: SorterType = (sortable: SortableType, ascending: boolean = true
       let linx = left
       let rinx = right
       let pivot = (left + right) >> 1
+      /** 절반으로 나누어(pivot) 왼쪽과 오른쪽을 정렬한다. */
       LOOP1: do {
         LOOP2: do {
           comp = sortable.compare(linx, pivot);
@@ -223,7 +226,7 @@ const quicksort: SorterType = (sortable: SortableType, ascending: boolean = true
 }
 
 const heapsort: SorterType = (sortable: SortableType, ascending: boolean = true) => {
-
+  /** 힙트리 생성 */
   const _make_heap = (sortable: SortableType, top: number, size: number, ascending: boolean) => {
     let pos = size >> 1
     /** make heap tree from base of the tree */
@@ -231,7 +234,7 @@ const heapsort: SorterType = (sortable: SortableType, ascending: boolean = true)
       _heapify(sortable, inx, pos, size, ascending)
     }
   }
-
+  /** 힙트리 정렬 */
   const _heapify = (sortable: SortableType, inx: number, pos: number, size: number, ascending: boolean) => {
     pos = pos + 1
     let is_bin = false
@@ -268,12 +271,14 @@ const heapsort: SorterType = (sortable: SortableType, ascending: boolean = true)
   {
     let pos: number
     let size = sortable.length()
+    /** 최초 힙트리를 정렬한다. */
     _make_heap(sortable, 0, size, ascending)
     while (size > 1) {
-      /** take node on top */
       size -= 1
       pos = size >> 1
+      /** 최상위 노드를 완료로 보내고 최하위 노드를 최상위 로 보낸다  */
       sortable.swap(0, size)
+      /** 다시 힙트리를 정렬한다 */
       _heapify(sortable, 1, pos, size, ascending)
     }
   }
@@ -284,7 +289,7 @@ const Sorter = {
   selectionsort,
   selectionsort2,
   quicksort,
-  heapsort
+  heapsort,
 }
 
 export {
